64397cb5c998626b404a07ce8e44ee3e7dcb9ab3
Zuhaib <theshahzuhaib96@gmail.com>
2021-12-19 16:09:56 +0530

    Add V2 files

---
+++ README.md
@@ -0,0 +1,11 @@
+# Still in making.
+
+## A full stack web app, where art can be bought with the option given to the seller to convert their art into an NFT
+
+###### Working on phase 1, which includes:
+
+-  _Completing the necessary Front-End work_
+
+-  _Adding Authentication_
+
+-  _Working on the design_
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+const port = process.env.PORT || 3000;
+// app.get('/', (req, res) => {
+//    res.sendFile(__dirname + '/index.html');
+// });
+app.listen(port, () => {
+   console.log('Server running on port 3000');
+});
+
+// how to use static folder
