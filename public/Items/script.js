10872542a1d83d0cd529a5a21e72b4e44e045d4f
Zuhaib <theshahzuhaib96@gmail.com>
2021-12-19 16:17:10 +0530

    remove double click

--- public/Items/script.js
+++ public/Items/script.js
@@ -1,100 +1,81 @@
 //Go Back
-function OpenProduct(i){
-  var x = document.getElementById("lightbox_blanket");
-  if (x.style.visibility === "hidden") {
-    x.style.visibility = "visible";
-  } else {
-    x.style.visibility = "hidden";
-  }
+function OpenProduct(i) {
+   var cardManufacturer = document.querySelectorAll(
+      `.product-popup-manufacturer[data-item='${i}']`
+   );
+   const manufacturer = cardManufacturer[0].innerHTML;
 
-  var cardManufacturer = document.querySelectorAll(`.product-popup-manufacturer[data-item='${i}']`);
-  const manufacturer = cardManufacturer[0].innerHTML;
+   const man = document.getElementsByClassName('product-manufacturer');
+   man[0].innerHTML = manufacturer;
 
-  const man = document.getElementsByClassName('product-manufacturer');
-  man[0].innerHTML = manufacturer;
+   var cardTitle = document.querySelectorAll(
+      `.product-title[data-item='${i}']`
+   );
 
-  var cardTitle = document.querySelectorAll(`.product-title[data-item='${i}']`);
- 
-  const title = cardTitle[0].innerHTML;
-  //console.log(title);
-  
-  const el = document.getElementsByClassName('product-popup-title');
-  
-  //console.log(el[0].innerHTML);
-  el[0].innerHTML = title
-  //onsole.log(el[0].innerHTML);
+   const title = cardTitle[0].innerHTML;
 
+   const el = document.getElementsByClassName('product-popup-title');
 
-  
-  
+   el[0].innerHTML = title;
 
-  var i = $('.product-image[item-data="'+i+'"] img');
-  var lbi = $('.lightbox-blanket .product-image img');
-  
-  $(lbi).attr("src", $(i).attr("src"));  
-  $(".lightbox-blanket").toggle();
+   var i = $('.product-image[item-data="' + i + '"] img');
+   var lbi = $('.lightbox-blanket .product-image img');
 
-  
-  
+   $(lbi).attr('src', $(i).attr('src'));
+   $('.lightbox-blanket').toggle();
 
-  $("#product-quantity-input").val("0");
-  CalcPrice (0);
-  
+   $('#product-quantity-input').val('0');
+   CalcPrice(0);
 }
-function GoBack(){
-  $(".lightbox-blanket").toggle();
-  var x = document.getElementById("lightbox_blanket");
-  if (x.style.visibility === "visible") {
-    x.style.visibility = "hidden";
-  } else {
-    x.style.visibility = "visible";
-  }
+function GoBack() {
+   $('.lightbox-blanket').toggle();
 }
 
 //Calculate new total when the quantity changes.
-function CalcPrice (qty){
-  var price = $(".product-price").attr("price-data");
-  var total = parseFloat((price * qty)).toFixed(2);
-  $(".product-checkout-total-amount").text(total);
+function CalcPrice(qty) {
+   var price = $('.product-price').attr('price-data');
+   var total = parseFloat(price * qty).toFixed(2);
+   $('.product-checkout-total-amount').text(total);
 }
 
 //Reduce quantity by 1 if clicked
-$(document).on("click", ".product-quantity-subtract", function(e){
-  var value = $("#product-quantity-input").val();
-  //console.log(value);
-  var newValue = parseInt(value) - 1;
-  if(newValue < 0) newValue=0;
-  $("#product-quantity-input").val(newValue);
-  CalcPrice(newValue);
+$(document).on('click', '.product-quantity-subtract', function (e) {
+   var value = $('#product-quantity-input').val();
+   //console.log(value);
+   var newValue = parseInt(value) - 1;
+   if (newValue < 0) newValue = 0;
+   $('#product-quantity-input').val(newValue);
+   CalcPrice(newValue);
 });
 
 //Increase quantity by 1 if clicked
-$(document).on("click", ".product-quantity-add", function(e){
-  var value = $("#product-quantity-input").val();
-  //console.log(value);
-  var newValue = parseInt(value) + 1;
-  $("#product-quantity-input").val(newValue);
-  CalcPrice(newValue);
+$(document).on('click', '.product-quantity-add', function (e) {
+   var value = $('#product-quantity-input').val();
+   //console.log(value);
+   var newValue = parseInt(value) + 1;
+   $('#product-quantity-input').val(newValue);
+   CalcPrice(newValue);
 });
 
-$(document).on("blur", "#product-quantity-input", function(e){
-  var value = $("#product-quantity-input").val();
-  //console.log(value);
-  CalcPrice(value);
+$(document).on('blur', '#product-quantity-input', function (e) {
+   var value = $('#product-quantity-input').val();
+   //console.log(value);
+   CalcPrice(value);
 });
 
-
-function AddToCart(e){
-  e.preventDefault();
-  var qty = $("#product-quantity-input").val();
-  if(qty === '0'){return;}
-  var toast = '<div class="toast toast-success">Added '+ qty +' to cart.</div>';  
-  $("body").append(toast);
-  setTimeout(function(){ 
-  $(".toast").addClass("toast-transition");
-    }, 100);
-  setTimeout(function(){      
-    $(".toast").remove();
-  }, 3500);
+function AddToCart(e) {
+   e.preventDefault();
+   var qty = $('#product-quantity-input').val();
+   if (qty === '0') {
+      return;
+   }
+   var toast =
+      '<div class="toast toast-success">Added ' + qty + ' to cart.</div>';
+   $('body').append(toast);
+   setTimeout(function () {
+      $('.toast').addClass('toast-transition');
+   }, 100);
+   setTimeout(function () {
+      $('.toast').remove();
+   }, 3500);
 }
-
